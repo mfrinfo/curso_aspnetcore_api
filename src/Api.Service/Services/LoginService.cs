30fe702d3534eff4df63435e7f9c6880f7272699
mfrinfo <mfrinfo@gmail.com>
2019-08-24 14:51:31 -0300

    --

--- src/Api.Service/Services/LoginService.cs
+++ src/Api.Service/Services/LoginService.cs
@@ -13,96 +13,93 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Service.Services
 {
-    public class LoginService : ILoginService
+  public class LoginService : ILoginService
+  {
+    private IUserRepository _repository;
+    private SigningConfigurations _signingConfigurations;
+    private TokenConfigurations _tokenConfigurations;
+    private IConfiguration _configuration { get; }
+
+    public LoginService(IUserRepository repository,
+                        IConfiguration configuration,
+                        SigningConfigurations signingConfigurations,
+                        TokenConfigurations tokenConfigurations)
     {
-        private IUserRepository _repository;
-        public SigningConfigurations _signingConfigurations;
-        public TokenConfigurations _tokenConfigurations;
-        public IConfiguration _configuration { get; }
-
-        public LoginService(IUserRepository repository,
-                            IConfiguration configuration,
-                            SigningConfigurations signingConfigurations,
-                            TokenConfigurations tokenConfigurations)
-        {
-            _repository = repository;
-            _signingConfigurations = signingConfigurations;
-            _configuration = configuration;
-            _tokenConfigurations = tokenConfigurations;
-        }
-        public async Task<object> FindByLogin(LoginDto user)
-        {
-            var baseUser = new UserEntity();
-            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
-            {
-                baseUser = await _repository.FindByLogin(user.Email);
-                if (baseUser == null)
-                {
-                    return new
-                    {
-                        authenticated = false,
-                        message = "Falha ao autenticar"
-                    };
-                }
-                else
-                {
-                    ClaimsIdentity identity = new ClaimsIdentity(
-                        new GenericIdentity(user.Email),
-                        new[]
-                        {
-                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //jti O id do token
-                            new Claim(JwtRegisteredClaimNames.UniqueName, user.Email),
-                        }
-                    );
-                    DateTime createDate = DateTime.Now;
-                    DateTime expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds);  //60 segundos = 1 minuto
-                    /* uma instância do tipo JwtSecurityTokenHandler 
-                       (namespace System.IdentityModel.Tokens.Jwt). 
-                       Este último elemento é então transformado em uma string por 
-                       meio do método WriteToken                    
-                    */
-                    var handler = new JwtSecurityTokenHandler();
-                    string token = CreateToken(identity, createDate, expirationDate, handler);
-                    return SuccessObject(createDate, expirationDate, token, user);
-                }
-            }
-            else
-            {
-                return new
-                {
-                    authenticated = false,
-                    message = "Falha ao autenticar"
-                };
-            }
-        }
-
-        private string CreateToken(ClaimsIdentity identity, DateTime createDate, DateTime expirationDate, JwtSecurityTokenHandler handler)
-        {
-            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
-            {
-                Issuer = _tokenConfigurations.Issuer,
-                Audience = _tokenConfigurations.Audience,
-                SigningCredentials = _signingConfigurations.SigningCredentials,
-                Subject = identity,
-                NotBefore = createDate,
-                Expires = expirationDate,
-            });
-
-            var token = handler.WriteToken(securityToken);
-            return token;
-        }
-
-        private object SuccessObject(DateTime createDate, DateTime expirationDate, string token, LoginDto user)
-        {
-            return new
-            {
-                authenticated = true,
-                created = createDate.ToString("yyyy-MM-dd HH:mm:ss"),
-                expiration = expirationDate.ToString("yyyy-MM-dd HH:mm:ss"),
-                acessToken = token,
-                userName = user.Email,
-                message = "Usuário Logado com sucesso"
-            };
-        }
+      _repository = repository;
+      _signingConfigurations = signingConfigurations;
+      _configuration = configuration;
+      _tokenConfigurations = tokenConfigurations;
     }
+    public async Task<object> FindByLogin(LoginDto user)
+    {
+      var baseUser = new UserEntity();
+      if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+      {
+        baseUser = await _repository.FindByLogin(user.Email);
+        if (baseUser == null)
+        {
+          return new
+          {
+            authenticated = false,
+            message = "Falha ao autenticar"
+          };
+        }
+        else
+        {
+          var identity = new ClaimsIdentity(
+              new GenericIdentity(user.Email),
+              new[]
+              {
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //jti O id do token
+                    new Claim(JwtRegisteredClaimNames.UniqueName, user.Email),
+              }
+          );
+          DateTime createDate = DateTime.Now;
+          DateTime expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds);  //60 segundos = 1 minuto
+
+          var handler = new JwtSecurityTokenHandler();
+          string token = CreateToken(identity, createDate, expirationDate, handler);
+          return SuccessObject(createDate, expirationDate, token, baseUser);
+        }
+      }
+      else
+      {
+        return new
+        {
+          authenticated = false,
+          message = "Falha ao autenticar"
+        };
+      }
+    }
+
+    private string CreateToken(ClaimsIdentity identity, DateTime createDate, DateTime expirationDate, JwtSecurityTokenHandler handler)
+    {
+      var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+      {
+        Issuer = _tokenConfigurations.Issuer,
+        Audience = _tokenConfigurations.Audience,
+        SigningCredentials = _signingConfigurations.SigningCredentials,
+        Subject = identity,
+        NotBefore = createDate,
+        Expires = expirationDate,
+      });
+
+      var token = handler.WriteToken(securityToken);
+      return token;
+    }
+
+    private object SuccessObject(DateTime createDate, DateTime expirationDate, string token, UserEntity user)
+    {
+      return new
+      {
+        authenticated = true,
+        created = createDate.ToString("yyyy-MM-dd HH:mm:ss"),
+        expiration = expirationDate.ToString("yyyy-MM-dd HH:mm:ss"),
+        acessToken = token,
+        userName = user.Email,
+        name = user.Name,
+        message = "Usuário Logado com sucesso"
+      };
+    }
+  }
 }
