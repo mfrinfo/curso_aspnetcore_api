860ab8135e63ec9327173624782e19e9f1fc2c99
mfrinfo <mfrinfo@gmail.com>
2020-01-30 22:00:47 -0300

    Migração da Versão 3.0 para 3.1

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -13,72 +13,65 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
-namespace Application
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Application {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
 
-            ConfigureService.ConfigureDependenciesService(services);
-            ConfigureRepository.ConfigureDependenciesRepository(services);
+            ConfigureService.ConfigureDependenciesService (services);
+            ConfigureRepository.ConfigureDependenciesRepository (services);
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "ASPNETCORE 3.0 - 2019",
-                    Version = "v1",
-                    Description = "Exemplo de API REST criada no curso",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Marcos Fabricio Rosa",
-                        Email = string.Empty,
-                        Url = new Uri("https://github.com/mfrinfo"),
-                    },
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo {
+                    Title = "ASPNETCORE 3.1 - 2020",
+                        Version = "v1",
+                        Description = "Exemplo de API REST criada no curso",
+                        Contact = new OpenApiContact {
+                            Name = "Marcos Fabricio Rosa",
+                                Email = string.Empty,
+                                Url = new Uri ("https://github.com/mfrinfo"),
+                        },
                 });
 
                 //Colocar JWT no Swagger
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
+                c.AddSecurityDefinition ("Bearer", new OpenApiSecurityScheme {
                     Description = "Entre com o Token JWT",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey
                 });
 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement{
-                    { new OpenApiSecurityScheme { Reference = new OpenApiReference
+                c.AddSecurityRequirement (new OpenApiSecurityRequirement {
                     {
-                        Id = "Bearer",
-                        Type = ReferenceType.SecurityScheme
-                    }}, new List<string>()}
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Id = "Bearer",
+                                    Type = ReferenceType.SecurityScheme
+                            }
+                        }, new List<string> ()
+                    }
                 });
             });
 
-            var signingConfigurations = new SigningConfigurations();
-            services.AddSingleton(signingConfigurations); //Instancia Unica
+            var signingConfigurations = new SigningConfigurations ();
+            services.AddSingleton (signingConfigurations); //Instancia Unica
 
-            var tokenConfigurations = new TokenConfigurations();
-            new ConfigureFromConfigurationOptions<TokenConfigurations>(
-                Configuration.GetSection("TokenConfigurations"))
-                    .Configure(tokenConfigurations);
-            services.AddSingleton(tokenConfigurations);  //Instancia Unica
+            var tokenConfigurations = new TokenConfigurations ();
+            new ConfigureFromConfigurationOptions<TokenConfigurations> (
+                    Configuration.GetSection ("TokenConfigurations"))
+                .Configure (tokenConfigurations);
+            services.AddSingleton (tokenConfigurations); //Instancia Unica
 
-            services.AddAuthentication(authOptions =>
-            {
+            services.AddAuthentication (authOptions => {
                 authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(bearerOptions =>
-            {
+            }).AddJwtBearer (bearerOptions => {
                 var paramsValidation = bearerOptions.TokenValidationParameters;
                 paramsValidation.IssuerSigningKey = signingConfigurations.Key;
                 paramsValidation.ValidAudience = tokenConfigurations.Audience;
@@ -98,45 +91,40 @@ namespace Application
 
             // Ativa o uso do token como forma de autorizar o acesso
             // a recursos deste projeto
-            services.AddAuthorization(auth =>
-            {
-                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
-                    .RequireAuthenticatedUser().Build());
+            services.AddAuthorization (auth => {
+                auth.AddPolicy ("Bearer", new AuthorizationPolicyBuilder ()
+                    .AddAuthenticationSchemes (JwtBearerDefaults.AuthenticationScheme‌​)
+                    .RequireAuthenticatedUser ().Build ());
             });
 
             // services.AddMvc(Options => { Options.EnableEndpointRouting = false; })
             //         .AddNewtonsoftJson();
-            services.AddControllers()
-                    .AddNewtonsoftJson();
+            services.AddControllers ()
+                .AddNewtonsoftJson ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
             // Ativando middlewares para uso do Swagger
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
+            app.UseSwagger ();
+            app.UseSwaggerUI (c => {
                 c.RoutePrefix = string.Empty;
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.0");
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.0");
             });
 
             // Redireciona o Link para o Swagger, quando acessar a rota principal
-            var option = new RewriteOptions();
-            option.AddRedirect("^$", "swagger");
-            app.UseRewriter(option);
+            var option = new RewriteOptions ();
+            option.AddRedirect ("^$", "swagger");
+            app.UseRewriter (option);
             // app.UseMvc();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseRouting ();
+            app.UseAuthorization ();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
